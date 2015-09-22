312b944dcec431c4926428a4cac34775cda7e474
David R. Jones <david.jones@madwiremedia.com>
2015-09-21 19:18:56 -0600

    file_get / file_put contents

---
+++ files.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$a = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=denver,co");
+
+
+$b = json_decode($a, true);
+
+print_r($b);
+
+
+$c = "David wrote to the file ". date('D, d M Y H:i:s') . "\r\n";
+
+file_put_contents("file.log", $c, FILE_APPEND);
